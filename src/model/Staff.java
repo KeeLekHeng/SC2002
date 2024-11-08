979868fcc083ca6b9c31db5aa6d946636086af56
Liew Jia Wei <mjw.liew@gmail.com>
2024-11-08 14:17:03 +0800

    changed to staff class for doc,pharm,admin

---
+++ src/model/Staff.java
@@ -0,0 +1,39 @@
+package src.model;
+
+import src.model.enums.Role;
+import src.model.enums.Gender;
+import java.io.Serializable;
+
+public class Staff extends User implements Serializable {
+    public String name;
+    public int age;
+    public final Gender gender;
+
+    public Staff(int id, Role role,String name, Gender gender,int age) {
+        super(id,role);
+        this.name = name;
+        this.gender= gender;
+        this.age= age;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public int getAge () {
+        return this.age;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public Gender getGender () {
+        return this.gender;
+    }
+
+}
