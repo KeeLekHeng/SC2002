17f91df03ff3f89336b93dd7c656f8e5381d0113
Liew Jia Wei <mjw.liew@gmail.com>
2024-11-08 14:07:36 +0800

    fixed bug

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -13,7 +13,7 @@ public class Patient extends User implements Serializable {
         super(patientID,role);
 
         //doctorID in medical record set to default 0
-        this.medicalRecord = new MedicalRecord(0, name, dob, gender, phonenumber, email,bloodType); 
+        this.medicalRecord = new MedicalRecord(0,patientID, name, dob, gender, phonenumber, email,bloodType); 
     }
 
     public MedicalRecord getMedicalRecord () {
